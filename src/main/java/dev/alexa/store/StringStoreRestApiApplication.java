282d4b57a44424af5742dcd697291e65e66fd145
Alexa <alexadeleted@gmail.com>
2022-08-14 14:03:11 +0300

    initial commit

---
+++ src/main/java/dev/alexa/store/StringStoreRestApiApplication.java
@@ -0,0 +1,13 @@
+package dev.alexa.store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StringStoreRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StringStoreRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alexa/store/StringStoreRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexa.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StringStoreRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
